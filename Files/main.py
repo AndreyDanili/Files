5ca4de5821ecf8a45e8985854b97aa555b6c4b76
Andrey Danilishin <adanilishin@gmail.com>
2021-12-20 11:22:33 +0300

    Completion of task 3

--- Files/main.py
+++ Files/main.py
@@ -1,17 +1,25 @@
-files = ['1.txt', '2.txt', '3.txt']
-number_lines = {}
-for document in files:
-    with open(document) as file:
-        lines = 0
-        for line in file:
-            lines += 1
-        number_lines[document] = lines
-sorted_lines = dict(sorted(number_lines.items(), key=lambda x: x[1]))
+def get_number_lines():
+    files = ['1.txt', '2.txt', '3.txt']
+    number_lines = {}
+    for document in files:
+        with open(document) as file:
+            lines = 0
+            for line in file:
+                lines += 1
+            number_lines[document] = lines
+    sorted_lines = dict(sorted(number_lines.items(), key=lambda x: x[1]))
+    return sorted_lines
 
-with open('final_file.txt', 'w') as file:
-    for sorted_file in sorted_lines:
-        file.write(f'{sorted_file}\n')
-        file.write(f'{sorted_lines[sorted_file]}\n')
-        with open(sorted_file) as document:
-            for line in document:
-                file.write(f'{line.strip()}\n')
+
+def get_merged_file(file_name):
+    counted_lines = get_number_lines()
+    with open(file_name, 'w') as file:
+        for sorted_file in counted_lines:
+            file.write(f'{sorted_file}\n')
+            file.write(f'{counted_lines[sorted_file]}\n')
+            with open(sorted_file) as document:
+                for line in document:
+                    file.write(f'{line.strip()}\n')
+
+
+get_merged_file('final_file.txt')
